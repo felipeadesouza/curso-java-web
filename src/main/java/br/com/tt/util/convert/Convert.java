dcc136c882dcbb27fba58b68aa9d580808a29369
sala03 <sala03@targettrust.local>
2017-10-09 21:37:10 -0300

    logica do cpf-nao funciona

--- src/main/java/br/com/tt/util/convert/Convert.java
+++ src/main/java/br/com/tt/util/convert/Convert.java
@@ -6,12 +6,18 @@ import org.springframework.stereotype.Service;
 public class Convert {
 
 	public String cpf(String cpf) {
+
 		String bloco1 = cpf.substring(0, 3);
 		String bloco2 = cpf.substring(3, 6);
 		String bloco3 = cpf.substring(6, 9);
 		String bloco4 = cpf.substring(9, 11);
 
-		return bloco1 + "." + bloco2 + "." + bloco3 + "-" + bloco4;
+		cpf = bloco1 + "." + bloco2 + "." + bloco3 + "-" + bloco4;
+		
+		//usar isto no index do cliente para formatar
+		//<td th:text="${beans.cv.cpf(c.cpf)}">
+
+		return cpf;
 	}
 
 }
